e96513f7735a9b59e2271dae4d3bfe9e1dee7803
浦田　幸嗣 <btz0727@me.com>
2018-03-08 20:41:19 +0900

    oath → oauthへ訂正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def callback_from(provider)
     provider = provider.to_s
 
-    @user = User.find_for_oath(request.env['omniauth.auth'])
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
