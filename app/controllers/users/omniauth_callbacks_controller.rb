a57ef9c335d6e95ce8d6e43fad3fa19a455e49b9
浦田　幸嗣 <btz0727@me.com>
2018-03-08 15:30:05 +0900

    Add end

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,4 +16,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
+  end
 end
