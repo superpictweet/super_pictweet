90d3d5ffb19de705d2616c8887a9654533d5eca4
Masato Komatsubara <masa@Masato-no-MacBook.local>
2018-03-09 23:17:26 +0900

    バリデーションの変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,4 @@
 class Tweet < ApplicationRecord
-  validates :image, allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/
-  validates :image_or_text, presence: true
-
-  def image_or_text
-    image.presence || text.presence
-  end
+  validates :image, url_format: true
+  validates_with PostTweetValidator
 end
