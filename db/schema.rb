978a2f3b4b7a489d99890228bb72595207ab1ca2
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-03-12 00:51:54 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180308060631) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
