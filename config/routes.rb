4fc79f335d0209963236d47cdaa72a6dbe66177b
浦田　幸嗣 <btz0727@me.com>
2018-03-13 16:00:33 +0900

    Add destroy routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweets#index"
 
-  resources :tweets, only: [:new, :create]
+  resources :tweets, only: [:new, :create, :destroy]
 end
