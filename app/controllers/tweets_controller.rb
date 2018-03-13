2371c9caaa756ff338de5bd13efa546b785af9cd
浦田　幸嗣 <btz0727@me.com>
2018-03-13 15:52:43 +0900

    Add create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,5 +8,16 @@ class TweetsController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
