20cbf12c0c08d8204fdbe79d3fa84bf07ba4a760
浦田　幸嗣 <btz0727@me.com>
2018-03-13 16:02:56 +0900

    Add destroy action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,6 +16,13 @@ class TweetsController < ApplicationController
     end
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
