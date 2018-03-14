045a3dd36989a8541139928181d161dcb35a5cce
浦田　幸嗣 <btz0727@me.com>
2018-03-14 15:10:03 +0900

    add move_to_index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   before_action :set_tweet, only: [:show, :destroy, :edit, :update]
+  before_action :move_to_index, except: :index
 
   def index
     @tweets = Tweet.all
@@ -45,5 +46,8 @@ class TweetsController < ApplicationController
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+    def move_to_index
+    redirect_to action: 'index' unless user_signed_in?
   end
 end
