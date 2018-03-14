4dacfa855c38b55df33230667c3bcbbf593a435c
浦田　幸嗣 <btz0727@me.com>
2018-03-14 17:34:36 +0900

    画像リサイズ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,11 +27,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
+  process resize_to_fit: [700, 600]
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
