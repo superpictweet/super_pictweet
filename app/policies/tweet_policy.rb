d229e85502eb68e3a6f5a01ae59621fcf80bdbda
浦田　幸嗣 <btz0727@me.com>
2018-03-16 16:24:14 +0900

    Create applicationとtweetのpoilcy

---
+++ app/policies/tweet_policy.rb
@@ -0,0 +1,39 @@
+class TweetPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user_id: user.id)
+      end
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    record.user_id == user.id || user.admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    true
+  end
+end
