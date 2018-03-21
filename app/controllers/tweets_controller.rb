288046814b286a988ef55abb252cb21f4292077d
浦田　幸嗣 <btz0727@me.com>
2018-03-21 21:16:44 +0900

    tweets_controllerにおける管理者権限の実装、update条件分岐、move_to_indexお引越し

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,8 @@
 class TweetsController < ApplicationController
 
-  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: :index
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  after_action :verify_authorized, except: [:index, :new, :create, :show]
 
   def index
     @tweets = Tweet.all
@@ -24,31 +25,33 @@ class TweetsController < ApplicationController
   end
 
   def edit
+    authorize @tweet
   end
 
   def update
-    if @tweet.user_id == current_user.id
-      @tweet.update(tweet_params)
+    authorize @tweet
+    if @tweet.update(tweet_params)
       redirect_to root_path
+    else
+     render :edit
     end
   end
 
   def destroy
-    if @tweet.user_id == current_user.id
-      @tweet.destroy
-    end
+    authorize @tweet
+    @tweet.destroy
   end
 
   private
+  def move_to_index
+    redirect_to action: 'index' unless user_signed_in?
+  end
+
   def tweet_params
     params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
   end
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
-  end
-
-  def move_to_index
-    redirect_to action: 'index' unless user_signed_in?
   end
 end
