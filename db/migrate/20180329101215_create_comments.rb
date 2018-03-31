7348591cffb0d90463b5718e8f76e4cf3decca7a
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-03-31 18:18:25 +0900

    remove index: true

--- db/migrate/20180329101215_create_comments.rb
+++ db/migrate/20180329101215_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :tweet, foreign_key: true, index: true
-      t.references :user, foreign_key: true, index: true
+      t.references :tweet, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
