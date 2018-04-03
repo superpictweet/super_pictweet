5773f19064a6034e000b289a5854751436668f54
浦田　幸嗣 <btz0727@me.com>
2018-04-03 16:58:31 +0900

    tweet modelにlikesとのassociationとlike?user methodを追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,12 @@ class Tweet < ApplicationRecord
   validates_with PostTweetValidator
 
   belongs_to :user
+  has_many :likes, dependent: :destroy
   mount_uploader :image, ImageUploader
 
   acts_as_taggable
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
