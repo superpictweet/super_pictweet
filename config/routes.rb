44d8c40816286f7ff89216e023b5ccb9f1d6d76d
浦田　幸嗣 <btz0727@me.com>
2018-04-03 17:00:09 +0900

    add likes routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "tweets#index"
-  resources :tweets
+  resources :tweets do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :users, only: :show
   resources :follows, only: :create
 end
