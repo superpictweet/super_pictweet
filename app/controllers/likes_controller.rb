33654d3cf440fd7df9eab3de5b1044957311f7cb
浦田　幸嗣 <btz0727@me.com>
2018-04-05 19:25:34 +0900

    delete user_tweet_params

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,19 +2,16 @@ class LikesController < ApplicationController
   before_action :set_likes, only: [:create, :destroy]
 
   def create
-    @like = Like.create(user_tweet_params)
+    @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
+
   end
 
   def destroy
-    like = Like.find_by(user_tweet_params)
+    like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
     like.destroy
   end
 
   private
-  def user_tweet_params
-    params.permit(user_id: current_user.id, tweet_id: params[:tweet_id])
-  end
-
   def set_likes
     @likes = Like.where(tweet_id: params[:tweet_id])
   end
