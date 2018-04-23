7f8cdf3575edec9fe0b0312dd97070c1e21f5fcd
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-04-23 18:05:39 +0900

    Like component with ajax Request

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,11 @@ class LikesController < ApplicationController
   before_action :set_tweet, only: [:create, :destroy]
 
   def create
-    @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
+    @like = Like.create(user_id: params[:user_id], tweet_id: params[:tweet_id])
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    like = Like.find_by(user_id: params[:user_id], tweet_id: params[:tweet_id])
     like.destroy
   end
 
--- app/javascript/bundles/components/tweets/Like.jsx
+++ app/javascript/bundles/components/tweets/Like.jsx
@@ -3,45 +3,102 @@ import React from 'react'
 export default class Like extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      count: this.props.count
+      user_id: this.props.user_id,
+      tweet_id: this.props.tweet_id,
+      count: this.props.count,
+      like_id: this.props.like_id,
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+
+    if (this.state.like_id)
+      this.sendDeleteRequest().then(
+        () => {
+          this.setState(prevState => ({
+            count: prevState.count - 1,
+            like_id: !prevState.like_id
+          }));
+        },
+        () => {
+          return "failed"
+        }
+      )
+    else {
+      this.sendPostRequest().then(
+        () => {
+          this.setState(prevState => ({
+            count: prevState.count + 1,
+            like_id: !prevState.like_id
+          }));
+        },
+        () => {
+          return "failed"
+        }
+      )
     }
   }
 
+  sendPostRequest() {
+    return $.ajax({
+      url: "tweets/" + this.state.tweet_id + "/likes",
+      type: 'POST',
+      data: {
+        user_id: this.state.user_id
+      }
+    })
+  }
+
+  sendDeleteRequest() {
+    return $.ajax({
+      url: "tweets/" + this.state.tweet_id + "/likes/" + this.state.like_id,
+      type: 'DELETE',
+      data: {
+        user_id: this.state.user_id
+      }
+    })
+  }
+
+  // TODO: CSS in JSを入れたら、templateの切り替えにanimation入れたい
   render() {
-    if (this.state.count === 0) {
-      return LikedTemplate(this.props)
+    if (this.state.like_id) {
+      return LikedTemplate(this.state.count, this.handleClick)
     } else {
-      return NotLikedTemplate(this.props)
+      return NotLikedTemplate(this.state.count, this.handleClick)
     }
   }
 }
 
 
-function LikedTemplate (props) {
+
+function LikedTemplate (count, clickFunc) {
   return (
-    <div class="ui labeled button" tabindex="0">
+    <div class="ui labeled button" onClick={clickFunc}>
       <div class="ui red button">
         <i class="heart icon"></i> Like
       </div>
       <a class="ui basic red left pointing label">
         <span>
-          { props.count }
+          { count }
         </span>
       </a>
     </div>
   )
 }
 
-function NotLikedTemplate (props) {
+function NotLikedTemplate (count, clickFunc) {
   return (
-    <div class="ui labeled button" tabindex="0">
+    <div class="ui labeled button" onClick={clickFunc}>
       <div class="ui button">
         <i class="heart icon"></i> Like
       </div>
       <a class="ui basic gray left pointing label">
         <span>
-          { props.count }
+          { count }
         </span>
       </a>
     </div>
