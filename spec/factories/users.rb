656244d2e14926018e224e08a596e5b82aae9814
浦田　幸嗣 <btz0727@me.com>
2018-04-29 20:06:06 +0900

    add factory :admin and role normalとadminを分けた

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,16 @@ FactoryBot.define do
     password password
     password_confirmation password
     provider 'facebook'
+    role 'normal'
+  end
+
+  factory :admin, class: User do
+    password = Faker::Internet.password(8)
+    name Faker::Name.name
+    initialize_with { User.find_or_create_by(email: Faker::Internet.email)}
+    password password
+    password_confirmation password
+    provider 'facebook'
+    role 'admin'
   end
 end
