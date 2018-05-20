35aab7d851eedc60f232b299cf96c63b17d47677
浦田　幸嗣 <btz0727@me.com>
2018-05-20 16:04:57 +0900

    add trait invalid

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     text Faker::Lorem.sentence
     image File.open("#{Rails.root}/public/images/IMG_2545.jpg")
     user
+
+    trait :invalid do
+      text nil
+      image nil
+    end
   end
 end
