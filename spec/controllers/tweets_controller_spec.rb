8acc347d8f4c5cff716ce0cb32c707fe3617b598
浦田　幸嗣 <btz0727@me.com>
2018-05-20 13:56:42 +0900

    add test of update action

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -73,7 +73,6 @@ describe TweetsController, type: :controller do
     end
 
     context "when user login and failed saving tweet" do
-
       before do
         login_user user
       end
@@ -84,6 +83,47 @@ describe TweetsController, type: :controller do
       it "renders the :index template" do
         get :index, params: { id: user.id }
         expect(response).to render_template :index
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context "as an authorized user" do
+      before do
+        login_admin admin
+      end
+
+      it "updates a tweet" do
+        tweet = create(:tweet, text: "Past Tweet")
+        tweet_params = attributes_for(:tweet, text: "New Tweet")
+        patch :update, params: { id: tweet.id, tweet: tweet_params }
+        expect(tweet.reload.text).to eq "New Tweet"
+      end
+    end
+
+    context "as an current user" do
+      before do
+        login_user user
+      end
+
+      it "updates the tweet" do
+        tweet = create(:tweet, text: "Past Tweet", user_id: user.id)
+        tweet_params = attributes_for(:tweet, text: "New Tweet")
+        patch :update, params: { id: tweet.id,  tweet: tweet_params }
+        expect(tweet.reload.text).to eq "New Tweet"
+      end
+    end
+
+    context "as a guest" do
+      before do
+        login_user user
+      end
+
+      it "does not update the tweet" do
+        tweet = create(:tweet, text: "Past Tweet")
+        tweet_params = attributes_for(:tweet, text: "New Tweet")
+        patch :update, params: { id: tweet.id,  tweet: tweet_params }
+        expect(tweet.reload.text).to eq "Past Tweet"
       end
     end
   end
